c2eff49135f53c777a5b1f6efbf4676bf23109e3
Ugurkan Kaya <ugurkankya@gmail.com>
2023-02-06 20:20:37 +0100

    use template directive for each group

--- pages/search.vue
+++ pages/search.vue
@@ -12,83 +12,101 @@
 
               <div class="row">
 
-                <div class="col-md-4" v-for="groupOption in property.groupOptions">
-                  <div class="mb-3 form-check" v-if="groupOption.type === 'checkbox'">
-                    <input type="checkbox" class="form-check-input" id="option1">
-                    <label class="form-check-label" for="option1">{{ groupOption.name }}</label>
-                  </div>
+                <template v-for="groupOption in property.groupOptions">
+                  <template v-if="groupOption.type === 'checkbox'">
+                    <div class="col-md-3 col-sm-6">
+                      <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" :id="`checkbox-${groupOption.id}`">
+                        <label class="form-check-label" :for="`checkbox-${groupOption.id}`">{{ groupOption.name }}</label>
+                      </div>
+                    </div>
+                  </template>
 
-                  <div class="mb-3" v-if="groupOption.type === 'select'">
-                    <label for="selectManufacturer" class="form-label">{{ groupOption.name }}</label>
-                    <select id="selectManufacturer" class="form-select">
-                      <option v-for="optionValue in groupOption.optionValues">{{ optionValue.value }}</option>
-                    </select>
-                  </div>
+                  <template v-if="groupOption.type === 'select'">
+                    <div class="col-md-3 col-sm-6">
+                      <div class="mb-3">
+                        <label :for="`select-${groupOption.id}`" class="form-label">{{ groupOption.name }}</label>
+                        <select :id="`select-${groupOption.id}`" class="form-select">
+                          <option v-for="optionValue in groupOption.optionValues">{{ optionValue.value }}</option>
+                        </select>
+                      </div>
+                    </div>
+                  </template>
 
-                  <div v-if="groupOption.type === 'checkboxGroup'">
+                  <template v-if="groupOption.type === 'checkboxGroup'">
                     <label>{{ groupOption.name }}</label>
 
-                    <div class="mb-3 form-check" v-for="optionValue in groupOption.optionValues">
-                      <input type="checkbox" class="form-check-input" id="option1">
-                      <label class="form-check-label" for="option1">{{ optionValue.value }}</label>
+                    <div class="col-md-4" v-for="optionValue in groupOption.optionValues">
+                      <div class="mb-3 form-check">
+                        <input type="checkbox"
+                               class="form-check-input"
+                               :id="`checkbox-${groupOption.id}-option-value-${optionValue.id}`">
+                        <label class="form-check-label"
+                               :for="`checkbox-${groupOption.id}-option-value-${optionValue.id}`">
+                          {{ optionValue.value }}
+                        </label>
+                      </div>
                     </div>
-                  </div>
+                  </template>
 
-                  <div v-if="groupOption.type === 'selectRange'">
-
+                  <template v-if="groupOption.type === 'selectRange'">
                     <div class="row">
                       <label class="font-bold">{{ groupOption.name }}</label>
 
-                      <div class="col">
+                      <div class="col-md-3 col-sm-6">
                         <select :id="`selectRange-${groupOption.id}-from`" class="form-select">
                           <option selected>von</option>
                           <option v-for="optionValue in groupOption.optionValues">{{ optionValue.value }}</option>
                         </select>
                       </div>
 
-                      <div class="col">
+                      <div class="col-md-3 col-sm-6">
                         <select :id="`selectRange-${groupOption.id}-to`" class="form-select">
                           <option selected>bis</option>
                           <option v-for="optionValue in groupOption.optionValues">{{ optionValue.value }}</option>
                         </select>
                       </div>
-
                     </div>
+                  </template>
 
-                  </div>
-
-                  <div class="mb-3" v-if="groupOption.type === 'multiSelect'">
-                    <div class="accordion" :id="`accordion-${groupOption.id}`">
-                      <div class="accordion-item">
-                        <h2 class="accordion-header" :id="`accordion-${groupOption.id}-heading`">
-                          <button class="accordion-button"
-                                  type="button"
-                                  data-bs-toggle="collapse"
-                                  :data-bs-target="`accordion-${groupOption.id}-collapse`"
-                                  aria-expanded="true"
-                                  :aria-controls="`accordion-${groupOption.id}-collapse`">
-                            {{ groupOption.name }}
-                          </button>
-                        </h2>
-                        <div :id="`accordion-${groupOption.id}-collapse`"
-                             class="accordion-collapse collapse show"
-                             :aria-labelledby="`accordion-${groupOption.id}-heading`"
-                             :data-bs-parent="`#accordion-${groupOption.id}`">
-                          <div class="accordion-body">
-                            <div class="mb-3 form-check" v-for="optionValue in groupOption.optionValues">
-                              <input type="checkbox" class="form-check-input"
-                                     :id="`accordion-${groupOption.id}-option-value-${optionValue.id}`">
-                              <label class="form-check-label" for="option1">{{ optionValue.value }}</label>
+                  <template v-if="groupOption.type === 'multiSelect'">
+                    <div class="mb-3">
+                      <div class="accordion" :id="`accordion-${groupOption.id}`">
+                        <div class="accordion-item">
+                          <h2 class="accordion-header" :id="`accordion-${groupOption.id}-heading`">
+                            <button class="accordion-button"
+                                    type="button"
+                                    data-bs-toggle="collapse"
+                                    :data-bs-target="`accordion-${groupOption.id}-collapse`"
+                                    aria-expanded="true"
+                                    :aria-controls="`accordion-${groupOption.id}-collapse`">
+                              {{ groupOption.name }}
+                            </button>
+                          </h2>
+                          <div :id="`accordion-${groupOption.id}-collapse`"
+                               class="accordion-collapse collapse show"
+                               :aria-labelledby="`accordion-${groupOption.id}-heading`"
+                               :data-bs-parent="`#accordion-${groupOption.id}`">
+                            <div class="accordion-body">
+                              <div class="mb-3 form-check" v-for="optionValue in groupOption.optionValues">
+                                <input type="checkbox" class="form-check-input"
+                                       :id="`accordion-${groupOption.id}-option-value-${optionValue.id}`">
+                                <label class="form-check-label"
+                                       :for="`accordion-${groupOption.id}-option-value-${optionValue.id}`">
+                                  {{ optionValue.value }}
+                                </label>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
+                  </template>
 
-                </div>
+                </template>
 
               </div>
+
             </div>
           </div>
         </div>
